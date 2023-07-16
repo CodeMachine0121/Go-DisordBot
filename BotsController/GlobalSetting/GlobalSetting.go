4297259c33cd74f2f0ff726eb96a019101338673
james.hsueh77 <james.hsueh@titansoft.com.sg>
2023-07-16 23:20:04 +0800

    factor: make code clean

---
+++ BotsController/GlobalSetting/GlobalSetting.go
@@ -0,0 +1,9 @@
+package GlobalSetting
+
+import "time"
+
+const VoiceChannelId string = "417666232851103748"
+const VaultAddress string = "http://127.0.0.1:8200"
+const VaultTokenPath string = "secret/data/discordBot"
+const LatencyTestRound int = 10
+const HighLatencyThreshold time.Duration = time.Second * 3
